20207392ac9f032188984a0de78f54404bbad06b
Эркин Камилов <erkimkamilov@gmail.com>
2020-01-10 07:53:16 +0500

    Добавьте файлы проекта.

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+import kivy
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+
+
+class ChatApp(App):
+    def build(self):
+        fl = FloatLayout()
+        fl.add_widget(Button(text = "Отправить",
+                             font_size = 23,
+                             size_hint = (0.25, 0.25),
+                             pos_hint = {'x':0.75, 'y':0.75}))
+        return fl
+
+if __name__ == "__main__":
+    ChatApp().run();
