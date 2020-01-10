19887abd04828b98745501b25944984ce80e7a45
Эркин Камилов <erkimkamilov@gmail.com>
2020-01-10 13:35:14 +0500

    Refactoring to kivy design language

--- main.py
+++ main.py
@@ -7,8 +7,28 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
 
-class ChatApp(App):
+Builder.load_string("""
+<RootWidget>:
+    Screen:
+        name: 'chatroom'
+
+        FloatLayout:
+            Button:
+                text: 'Отправить'
+                font_size: 20
+                size_hint: (0.15, 0.10)
+                pos_hint: {'x' : 0.825, 'y' : 0.05}
+
+""")
+
+class RootWidget(ScreenManager):
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
+
+class OldApp(App):
 
     def callback(self, instance):
         self.layout.add_widget(Button(text=str(10), size_hint_y=None, height=40))
@@ -27,6 +47,8 @@ class ChatApp(App):
         self.layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
         # Make sure the height is such that there is something to scroll.
         self.layout.bind(minimum_height=self.layout.setter('height'))
+        print(self.layout.size_hint)
+        
         for i in range(10):
             btn = Button(text=str(i), size_hint_y=None, height=40, on_press=self.callback)
             self.layout.add_widget(btn)
@@ -36,14 +58,21 @@ class ChatApp(App):
                                  pos_hint = {'x':0.975, 'y':0.975})
                                  #background_color = [1,1,1,1])
                                  #color = [1,1,1,1]) #debug
+        print(view.size_hint)
+        
         #view.add_widget(Label(text = 'HELLO!'))
-        view = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        view = ScrollView(size_hint=(0.95, 0.775),
+                          pos_hint = {'x':0.025, 'y':0.20})#, size=(Window.width, Window.height))
         view.add_widget(self.layout)
         fl.add_widget(view)
-        
-        return fl
+
+        return fl   # этот виджет идет в self.root
+
+class ChatApp(App):
+    def build(self):
+        return RootWidget()
     
 
 if __name__ == "__main__":
-    ChatApp().run();
-    ChatApp.layout.add_widget(Button(text=str(10), size_hint_y=None, height=40))
+    OldApp().run();
+    #ChatApp.layout.add_widget(Button(text=str(10), size_hint_y=None, height=40))
