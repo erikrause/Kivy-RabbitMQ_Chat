1a591d57a2facbd95740048c02e97a8fdb8bc6ce
Эркин Камилов <erkimkamilov@gmail.com>
2020-01-10 14:08:18 +0500

    GUI via kivy designer language

--- main.py
+++ main.py
@@ -22,51 +22,45 @@ Builder.load_string("""
                 size_hint: (0.15, 0.10)
                 pos_hint: {'x' : 0.825, 'y' : 0.05}
 
+            TextInput:
+                hint_text: "Введите сообщение"
+                size_hint: (0.775, 0.15)
+                pos_hint: {'x' : 0.025, 'y' : 0.025}
+            
+            ScrollView:
+                size_hint: (0.95, 0.775)
+                pos_hint: {'x':0.025, 'y':0.20}
+
+                GridLayout:
+                    orientation: "vertical"
+                    size_hint_y: None
+                    height: self.minimum_height  #<<<<<<<<<<<<<<<<<<<<
+                    row_default_height: 60
+                    cols:1
+
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
+                    Button:
 """)
 
 class RootWidget(ScreenManager):
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
 
-class OldApp(App):
 
-    def callback(self, instance):
-        self.layout.add_widget(Button(text=str(10), size_hint_y=None, height=40))
-
-    def build(self):
-        fl = FloatLayout()
-        fl.add_widget(Button(text = "Отправить",
-                             font_size = 20,
-                             size_hint = (0.15, 0.10),
-                             pos_hint = {'x' : 0.825, 'y' : 0.05}))
-        fl.add_widget(TextInput(hint_text = "Введите сообщение",
-                                size_hint = (0.775, 0.15),
-                                pos_hint = {'x' : 0.025, 'y' : 0.025}))
-
-
-        self.layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
-        # Make sure the height is such that there is something to scroll.
-        self.layout.bind(minimum_height=self.layout.setter('height'))
-        print(self.layout.size_hint)
-        
-        for i in range(10):
-            btn = Button(text=str(i), size_hint_y=None, height=40, on_press=self.callback)
-            self.layout.add_widget(btn)
-
-        self.layout.add_widget(Button(text=str(10), size_hint_y=None, height=40))
-        view = ScrollView(size_hint = (0.95, 0.775),
-                                 pos_hint = {'x':0.975, 'y':0.975})
-                                 #background_color = [1,1,1,1])
-                                 #color = [1,1,1,1]) #debug
-        print(view.size_hint)
-        
-        #view.add_widget(Label(text = 'HELLO!'))
-        view = ScrollView(size_hint=(0.95, 0.775),
-                          pos_hint = {'x':0.025, 'y':0.20})#, size=(Window.width, Window.height))
-        view.add_widget(self.layout)
-        fl.add_widget(view)
-
-        return fl   # этот виджет идет в self.root
 
 class ChatApp(App):
     def build(self):
@@ -74,5 +68,5 @@ class ChatApp(App):
     
 
 if __name__ == "__main__":
-    OldApp().run();
+    ChatApp().run();
     #ChatApp.layout.add_widget(Button(text=str(10), size_hint_y=None, height=40))
