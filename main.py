95abf3c76fe9372dab20cde193a54ea9bf209657
Эркин Камилов <erkimkamilov@gmail.com>
2020-01-11 10:03:33 +0500

    Add roating: service, private, public chats

--- main.py
+++ main.py
@@ -51,8 +51,25 @@ class ChatApp(App):
         channel = connection.channel()
 
         channel.queue_declare(queue=nickname)
-        channel.queue_bind(exchange='amq.fanout', queue=nickname)
+
+        # Public chat:
+        channel.queue_bind(exchange='amq.fanout',
+                           queue=nickname)
+
+        # Private chat:
+        channel.queue_bind(exchange='amq.direct',
+                           routing_key=self.nick,
+                           queue=nickname)
         
+        # Service chat:
+        channel.queue_bind(exchange='amq.direct',
+                           routing_key='service',
+                           queue=nickname)
+
+        # Private service chat:
+        channel.queue_bind(exchange='amq.direct',
+                           routing_key=self.nick + '_service',
+                           queue=nickname)
 
         return channel
 
@@ -70,33 +87,82 @@ class ChatApp(App):
     def recieve_msg(self, ch, method, properties, body):
 
         msg = body.decode('utf-8')
+        #############
+        # TO DELETE:
+        #recieve_nick = msg.split(" ")[1].replace(":", "")
+        #first_word = msg.split(" ")[2]
 
-        recieve_nick = msg.split(" ")[1].replace(":", "")
-        first_word = msg.split(" ")[2]
+        #if "@" in first_word:
+        #    self.parse_service(first_word, recieve_nick)
+            
+        ####################
+        color = '2980b9'
 
-        if "@" in first_word:
-            self.parse(first_word, recieve_nick)
+        msg_datetime = msg.split(" ")[0]
+        sender_nick = msg.split(" ")[1].replace(':', '')
+        first_word = msg.split(" ")[2].replace("@","")
+
+        is_service_msg = False
+        
+        if method.routing_key == 'service':
+            color = 'ff0000'
+            is_service_msg = True
+        if method.routing_key == self.nick:
+            color = 'ff00ff'
+        if method.routing_key == self.nick + '_service':
+            color = 'faaa00'
+            is_service_msg = True
 
         self.root.ids.chat_logs.text += (
-                '[b][color=2980b9] {}[/color][/b]\n'.format(self.esc_markup(msg))
+                '[b][color={}] {}[/color][/b]\n'.format(color, self.esc_markup(msg))
         )
         self.root.ids.view.scroll_y = 0
 
-    def parse(self, word, nick):
+        if first_word == "who_are_here?":
+            self.send_msg("@i_am_here! @" + sender_nick)
+
+        if is_service_msg:
+            ch.basic_ack(delivery_tag = method.delivery_tag)    # Удалить сообщение из очереди если оно сервисное
+
+    # Returns word and true if msg == service command
+    def parse_command(self, word):
 
         word = word.replace("@", "")
 
-        if nick != self.nick:
-            if word == "who_are_here?":
-                self.send_msg("@i_am_here!")
+        #if nick != self.nick:
+        if word == "who_are_here?":
+            #self.send_msg("@i_am_here!")
+            return True
 
 
     def send_msg(self, text):
         #text = self.root.ids.message.text
-        message = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')+" "+self.nick+": "+text
+
+        words = text.split(" ")
+        exchange = 'amq.fanout'
+        routing_key = ''
+
+        # Проверка на сервисное сообщение
+        if "@" in words[0]:
+            exchange = 'amq.direct'
+
+            if words[0] == "@who_are_here?" or \
+               words[0] == "@i_am_here!":
+
+                routing_key = 'service'
+                if len(words) > 1:
+                    if "@" in words[1]:     # Если сервис адресован кому-то:
+                        routing_key = words[1].replace('@','') + '_service'
+            else:
+                routing_key = words[0].replace("@", "")   # routing_key = nickname address
             
-        self.channel.basic_publish(exchange='amq.fanout',
-	                          routing_key='',
+
+
+        message = datetime.datetime.fromtimestamp(time.time()).strftime('%Y.%m.%d_%H:%M:%S')+" "+self.nick+": "+text
+
+            
+        self.channel.basic_publish(exchange=exchange,
+	                          routing_key=routing_key,
 	                          body=message,
 	                          properties=pika.BasicProperties(
 	                          delivery_mode=2,  # make message persistent
